a69f9969f43746280a7ede4119984e229149b230
Tom Clark <tclark@redhat.com>
2015-07-07 12:48:51 -0400

    Add python client scripts

---
+++ client/iotrobot-client.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python2
+
+import paho.mqtt.client as mqtt
+import struct
+import time
+import curses
+import sys
+import math
+
+class IoTRobotClient:
+    def __init__(self, broker="localhost"):
+        """A program that acts as an MQTT client and simply writes all of the
+        robot-related values to the terminal.
+        """
+        self.broker = broker
+        self.sensors = {}
+        self.active_topic = ""
+        self.control_label = ""
+
+    def __call__(self, stdscr):
+        """Initialize an MQTT client and begin the main loop"""
+        self.stdscr = stdscr
+        self.stdscr.timeout(50)
+
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+
+        client = mqtt.Client("Robot Dashboard")
+        client.username_pw_set("admin", "admin")
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.connect(self.broker)
+        client.loop_start()
+
+        self.drive(client, 0.0, 0.0, 0.0, 0.0)
+        self.redraw()
+
+        while True:
+            key = stdscr.getch()
+
+            if key != -1:
+                if key in (curses.KEY_UP, ord('w')):
+                    self.drive(client, 1.0, 1.0, 1.0, 1.0)
+                    self.control_label = "Forward"
+                elif key in (curses.KEY_DOWN, ord('s')):
+                    self.drive(client, -1.0, -1.0, -1.0, -1.0)
+                    self.control_label = "Backward"
+                elif key in (curses.KEY_LEFT, ord('a')):
+                    self.drive(client, -1.0, -1.0, 1.0, 1.0)
+                    self.control_label = "Left"
+                elif key in (curses.KEY_RIGHT, ord('d')):
+                    self.drive(client, 1.0, 1.0, -1.0, -1.0)
+                    self.control_label = "Right"
+                elif key == ord('q'):
+                    self.drive(client, 0.0, 0.0, 0.0, 0.0)
+                    break
+                else:
+                    self.drive(client, 0.0, 0.0, 0.0, 0.0)
+                    self.control_label = "Stop"
+
+                self.redraw()
+
+            self.stdscr.refresh()
+
+        client.loop_stop()
+
+    def on_connect(self, client, data, rc, _):
+        """Subscribe to all of the robot/ MQTT topics"""
+        client.subscribe("robot/#", qos=2)
+        self.redraw()
+
+    def on_message(self, client, data, msg):
+        """Update whatever sensor was published and redraw the screen"""
+        self.active_topic = msg.topic
+
+        if len(msg.payload) == 1:
+            self.sensors[msg.topic] = ord(msg.payload[0])
+        elif len(msg.payload) == 4:
+            self.sensors[msg.topic], = struct.unpack("<f", msg.payload)
+
+        self.redraw()
+
+    def redraw(self):
+        """Render a list of known topics and their values using ncurses"""
+        self.stdscr.clear()
+        self.stdscr.box()
+        self.stdscr.addstr(0, 4, "Robot Dashboard")
+
+        self.stdscr.addstr(1, 2, "Control:")
+        self.stdscr.addstr(1, 11, self.control_label, curses.color_pair(2  ))
+
+        lines = len(self.sensors)
+        cols = max(map(len, self.sensors)) if self.sensors else 0
+        for i, topic in enumerate(sorted(self.sensors)):
+            self.stdscr.addstr(i+3, 2, topic, curses.color_pair(1))
+            self.stdscr.addstr(i+3, cols+3, "=", curses.A_NORMAL)
+            self.stdscr.addstr(i+3, cols+5, str(self.sensors[topic]),
+                curses.A_BOLD if topic == self.active_topic else curses.A_NORMAL)
+
+    def drive(self, client, frontLeft, backLeft, frontRight, backRight):
+        """Publish the desired speeds of all four motors"""
+        client.publish("robot/motor/frontLeft", bytearray(struct.pack("<f", frontLeft)))
+        client.publish("robot/motor/backLeft", bytearray(struct.pack("<f", backLeft)))
+        client.publish("robot/motor/frontRight", bytearray(struct.pack("<f", frontRight)))
+        client.publish("robot/motor/backRight", bytearray(struct.pack("<f", backRight)))
+
+if __name__ == "__main__":
+    curses.wrapper(IoTRobotClient(*sys.argv[1:]))
---
+++ client/iotrobot-opengl.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python2
+
+import paho.mqtt.client as mqtt
+import struct
+import time
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+import sys
+import math
+
+# Colors
+BASE03 = (0.0/255.0, 43.0/255.0, 54.0/255.0)
+BASE02 = (7.0/255.0, 54.0/255.0, 66.0/255.0)
+BASE01 = (88.0/255.0, 110.0/255.0, 117.0/255.0)
+BASE00 = (101.0/255.0, 123.0/255.0, 131.0/255.0)
+BASE0 = (131.0/255.0, 148.0/255.0, 150.0/255.0)
+BASE1 = (147.0/255.0, 161.0/255.0, 161.0/255.0)
+BASE2 = (238.0/255.0, 232.0/255.0, 213.0/255.0)
+BASE3 = (253.0/255.0, 246.0/255.0, 227.0/255.0)
+YELLOW = (181.0/255.0, 137.0/255.0, 0.0/255.0)
+ORANGE = (203.0/255.0, 75.0/255.0, 22.0/255.0)
+RED = (220.0/255.0, 50.0/255.0, 47.0/255.0)
+MAGENTA = (211.0/255.0, 54.0/255.0, 130.0/255.0)
+VIOLET = (108.0/255.0, 113.0/255.0, 196.0/255.0)
+BLUE = (38.0/255.0, 139.0/255.0, 210.0/255.0)
+CYAN = (42.0/255.0, 161.0/255.0, 152.0/255.0)
+GREEN = (133.0/255.0, 153.0/255.0, 0.0/255.0)
+
+class IoTRobotGraphicalClient:
+    dirty = True
+    width, height = None, None
+
+    def __init__(self, broker="localhost"):
+        self.sensors = {}
+        self.measurements = {}
+        self.on_resize(200, 200)
+
+        client = mqtt.Client("Robot OpenGL Client")
+        client.username_pw_set("admin", "admin")
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.connect(broker)
+        client.loop_start()
+
+
+    def run(self):
+        clock = pygame.time.Clock()
+
+        glClearColor(BASE02[0], BASE02[1], BASE02[2], 1.0)
+        glEnable(GL_DEPTH_TEST)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.VIDEORESIZE:
+                    self.on_resize(event.w, event.h)
+
+            self.redraw()
+            clock.tick(100)
+
+
+    def on_resize(self, width, height):
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height), DOUBLEBUF|OPENGL|RESIZABLE)
+
+
+    def on_connect(self, client, data, rc, _):
+        """Subscribe to all of the robot/ MQTT topics"""
+        client.subscribe("robot/#", qos=2)
+
+
+    def on_message(self, client, data, msg):
+        """Update whatever sensor was published and redraw the screen"""
+        value, = struct.unpack("<f", msg.payload)
+        self.sensors[msg.topic] = value
+
+        if msg.topic in ("robot/angle/roll", "robot/angle/pitch"):
+            self.dirty = True
+
+    def redraw(self):
+        if not self.dirty: return
+        self.dirty = False
+
+        if "robot/angle/roll" in self.sensors and "robot/angle/pitch" in self.sensors:
+            # Draw a box rotated by the calculated Euler angles
+            glViewport(0, 0, self.width, self.height)
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+            glPushMatrix()
+
+            glRotatef(self.sensors["robot/angle/pitch"], 1, 0, 0)
+            glRotatef(self.sensors["robot/angle/roll"], 0, 0, 1)
+
+            glBegin(GL_QUADS)
+            glColor3f(*RED)
+            glVertex3f(-0.4, -0.2, -0.6)
+            glVertex3f(-0.4, -0.2, +0.6)
+            glVertex3f(-0.4, +0.2, +0.6)
+            glVertex3f(-0.4, +0.2, -0.6)
+            glVertex3f(+0.4, +0.2, -0.6)
+            glVertex3f(+0.4, +0.2, +0.6)
+            glVertex3f(+0.4, -0.2, +0.6)
+            glVertex3f(+0.4, -0.2, -0.6)
+            glColor3f(*GREEN)
+            glVertex3f(-0.4, -0.2, -0.6)
+            glVertex3f(+0.4, -0.2, -0.6)
+            glVertex3f(+0.4, +0.2, -0.6)
+            glVertex3f(-0.4, +0.2, -0.6)
+            glVertex3f(+0.4, +0.2, +0.6)
+            glVertex3f(-0.4, +0.2, +0.6)
+            glVertex3f(-0.4, -0.2, +0.6)
+            glVertex3f(+0.4, -0.2, +0.6)
+            glColor3f(*BLUE)
+            glVertex3f(-0.4, -0.2, -0.6)
+            glVertex3f(+0.4, -0.2, -0.6)
+            glVertex3f(+0.4, -0.2, +0.6)
+            glVertex3f(-0.4, -0.2, +0.6)
+            glVertex3f(-0.4, +0.2, -0.6)
+            glVertex3f(+0.4, +0.2, -0.6)
+            glVertex3f(+0.4, +0.2, +0.6)
+            glVertex3f(-0.4, +0.2, +0.6)
+            glEnd()
+
+            glPopMatrix()
+
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    IoTRobotGraphicalClient(*sys.argv[1:]).run()
+
